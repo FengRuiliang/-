c3d806f26ff21faf575f97f6714108f596914e37
冯瑞亮 <fengruiliang@sjtu.edu.cn>
2018-11-30 17:33:25 +0800

    add minkowski function

--- Meshprint/supportor.cpp
+++ Meshprint/supportor.cpp
@@ -16,12 +16,25 @@ Supportor::~Supportor()
 
 void Supportor::add_support_point(std::vector<std::vector<std::vector<Segment*>>>* cnts)
 {
-	ClipperLib::ClipperOffset offsetor;
+
 	ClipperLib::Clipper cliper;
+	ClipperLib::ClipperOffset off;
+	Path pattern;
+	for (int i = 0; i < 18; i++)
+	{
+		pattern << IntPoint(384 * cos(i * 20), 384 * sin(i * 20));
+	}
+	Paths solution;
+	Paths under_paths;
+	Paths upper_paths;
+	Paths mink_sum;
 	for (int i = 2; i < cnts->size(); i++)
 	{
-		Paths under_paths;
-		for each (std::vector<Segment*> var in cnts->at(i-1))
+		under_paths.clear();
+		solution.clear();
+		upper_paths.clear();
+		mink_sum.clear();
+		for each (std::vector<Segment*> var in cnts->at(i - 1))
 		{
 			Path path;
 			for each (Segment* ptr_seg in var)
@@ -30,55 +43,76 @@ void Supportor::add_support_point(std::vector<std::vector<std::vector<Segment*>>
 			}
 			under_paths << path;
 		}
-		offsetor.Clear();
-		offsetor.AddPaths(under_paths, jtMiter, etClosedPolygon);
-		offsetor.Execute(under_paths, 384);
-		for (int j = 0; j < cnts->at(i).size(); j++)
+		
+		for each (std::vector<Segment*> var in cnts->at(i))
 		{
-			for (int k = 0; k < cnts->at(i)[j].size(); k++)
+			Path path;
+			for each (Segment* ptr_seg in var)
 			{
-				IntPoint p_1(cnts->at(i)[j][k]->get_v1().x()*1e3, cnts->at(i)[j][k]->get_v1().y()*1e3);
-				IntPoint p_2(cnts->at(i)[j][k]->get_v2().x()*1e3, cnts->at(i)[j][k]->get_v2().y()*1e3);
-				int pip1 = 0, pip2 = 0;
-				for each (Path path in under_paths)
-				{
-					pip1 += (int)(PointInPolygon(p_1, path) == 1);
-					pip2 += (int)(PointInPolygon(p_2, path) == 1);
-				}
-				//1 means inside the under polygon,
-				//-1 means cross the under polygon,
-				//0 means outside the under polygon
-				if (pip1 % 2 + pip2 % 2 != 1)
-				{
-					cnts->at(i)[j][k]->set_angle(0);
-				}
+				path << IntPoint(ptr_seg->get_v1().x()*1e3, ptr_seg->get_v1().y()*1e3);
 			}
+			upper_paths << path;
 		}
-		sample_support_point(cnts->at(i));
-	}
-}
-
-
-void Supportor::sample_support_point(std::vector<std::vector<Segment*>> upper)
-{
-	for each (std::vector<Segment*> poly in upper)
-	{
-		float length_left = 0;
-		for each(Segment* seg in poly)
+		off.Clear();
+		off.AddPaths(under_paths, jtSquare, etClosedPolygon);
+		off.Execute(solution, 384);
+		
+		cliper.Clear();
+		cliper.AddPaths(solution, ptClip, true);
+		cliper.AddPaths(upper_paths, ptSubject, true);
+		cliper.Execute(ctDifference, solution, pftNonZero, pftNonZero);
+		if (solution.size())
 		{
-			if (seg->get_angle() < 21)
+			qDebug() << "jump into sampling ";
+			MinkowskiSum(pattern, under_paths, mink_sum, true);
+			solution.clear();
+			cliper.Clear();
+			cliper.AddPaths(mink_sum, ptClip, true);
+			cliper.AddPaths(under_paths, ptSubject, true);
+			cliper.Execute(ctUnion, mink_sum, pftNonZero, pftNonZero);
+			cliper.Clear();
+			cliper.AddPaths(mink_sum, ptClip, true);
+			cliper.AddPaths(under_paths, ptSubject, true);
+			cliper.Execute(ctDifference, solution, pftNonZero, pftNonZero);
+			for (int j = 0; j < solution.size(); j++)
 			{
-				length_left += seg->get_length();
-				while (length_left>3.5)
+				for (int k = 0; k < solution[j].size(); k++)
 				{
-					length_left -= 3.5;
-					Vec3f p = seg->get_v2() - length_left*seg->get_normal();
-					sup_points->push_back(p);
+					IntPoint p1 = solution[j][k];
+					IntPoint p2 = solution[j][(k + 1) % solution[j].size()];
+					bool is_on_minkowskisum_1 = false;
+					bool is_on_minkowskisum_2 = false;
+					for each(Path poly in mink_sum)
+					{
+						if (PointInPolygon(p1, poly) == -1)
+						{
+							is_on_minkowskisum_1 = true;
+						}
+						if (PointInPolygon(p2, poly) == -1)
+						{
+							is_on_minkowskisum_2 = true;
+						}
+					}
+					if (!is_on_minkowskisum_2)
+					{
+						if (!is_on_minkowskisum_1)
+						{
+							Vec3f p = Vec3f(p1.X*1e-3, p1.Y*1e-3, i*0.09);
+							sup_points->push_back(p);
+						}
+						Vec3f pf(p1.X*1e-3, p1.Y*1e-3, i*0.09);
+						Vec3f pb(p2.X*1e-3, p2.Y*1e-3, i*0.09);
+						Vec3f vec = pb - pf;
+						float length_left = vec.length();
+						vec.normalize();
+						while (length_left > 3)
+						{
+							length_left -= 3;
+							Vec3f p = pf + 3.0f*vec;
+							sup_points->push_back(p);
+						}
+					}
 				}
-			}
-			else
-			{
-				length_left = 0;
 			}
 		}
 	}
